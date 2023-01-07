1d16829c779dabf87d7140ec02201b8b46dee5ac
Ralica1 <rasim.korajlic@gmail.com>
2023-01-07 01:20:29 +0100

    Learning to minimize and use css with webpack

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-// import your function
-import myName from './myName';
-
-function component() {
-  const element = document.createElement('div');
-
-  // use your function!
-  element.textContent = myName('Cody');
+  element.classList.add('hello');
   return element;
 }
 
